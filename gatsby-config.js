925a221b2c3f977c24c4fa1c963f8b78adafc941
Tomek <tomek@noito.co>
2022-01-08 20:17:17 +0100

    done examples from chapter Gatsby Basics

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteMetadata: {
+        siteUrl: "https://yourdomain.test",
+        title: "Frontend Masters Intro to Gatsby",
+        description: "Frontend Masters Intro to Gatsby course project",
+        image: "https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg"
+    },
+};
