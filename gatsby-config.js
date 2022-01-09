1a8caa7a739020c4f393fc31635858160c1bd1fc
Tomek <tomek@noito.co>
2022-01-09 20:15:29 +0100

    done examples from chapter Markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,4 +5,32 @@ module.exports = {
         description: "Frontend Masters Intro to Gatsby course project",
         image: "https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg"
     },
+    plugins: [
+        // plugin to add head meta tags to all pages
+        'gatsby-plugin-react-helmet',
+
+        // plugins used to enable creating MDX blog posts from the ./src/posts/ folder
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'posts',
+                path: `${__dirname}/src/posts`
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-page-creator',
+            options: {
+                path: `${__dirname}/src/posts`
+            }
+        },
+        {
+            resolve: 'gatsby-plugin-mdx',
+            options: {
+                defaultLayouts: {
+                    // posts as that's the name used in the gatsby-source-filesystem config
+                    posts: require.resolve('./src/components/post-layout.js'),
+                }
+            }
+        }
+    ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,46 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import Layout from '../components/layout.js';
 
+
+
 export default function IndexPage() {
+    const data = useStaticQuery(graphql`
+        query GetBlogPosts {
+            allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+            nodes {
+                id
+                slug
+                frontmatter {
+                title
+                description
+                date(fromNow: true)
+                }
+            }
+            }
+        }
+    `);
+
+    const posts = data.allMdx.nodes;
+
+
+
     return (
         <Layout>
             <h1>Hello Frontend Masters!</h1>
+
             <Link to="/about">About this site</Link>
+
+            <h2>Check out the lates blog posts</h2>
+
+            <ul>
+                { posts.map((post) => (
+                    <li key={ post.id }>
+                        <Link to={ post.slug }>{ post.frontmatter.title }</Link>{' '}
+                        <small>posted { post.frontmatter.date }</small>
+                    </li>
+                ))}
+            </ul>
         </Layout>
     );
 }
